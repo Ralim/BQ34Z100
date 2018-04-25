ddd3416b3266bc19da4828990629474fc9d027c7
Ben V. Brown <Ralim@Ralimtek.com>
2018-04-25 18:24:41 +1000

    Update setup.ino

--- examples/setup/setup.ino
+++ examples/setup/setup.ino
@@ -8,12 +8,12 @@ void setup() {
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW);
   //Auto calibration (Run this once on the bq34z100 to calibrate most of the internal registers
-  bq.setup(0x101,3,8000,12369);
+  bq.setup(0x101,3,8000,12369,1000);
   //Battery Chemistry
   //Number of series Batteries
   //Battery capacity in mAh
   //Current voltage on the pack in mV (eg 12.369V)
-  
+  // Current being applied to the pack for currentShunt Cal in mA (must be > 200mA)
   delay(200);
 }
 
